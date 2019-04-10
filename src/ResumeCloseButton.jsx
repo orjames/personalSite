8c98c923beaaff165f325b1c7e17b2ab56fb9b95
orjames <orjames@berkeley.edu>
2019-04-10 15:57:32 -0700

    working on resume div

--- src/ResumeCloseButton.jsx
+++ src/ResumeCloseButton.jsx
@@ -5,15 +5,13 @@ import XIcon from './icons/XIcon';
 
 const ResumeCloseButton = (props) => {
   return (
-    <div>
-      <div className='fadein'>
-        <div onClick={() => props.closeResume()} className='delete'>
-          <XIcon width={35} />
-        </div>
-        <div>
-          <Resume />
-          <span className='resume-close-link'>close</span>
-        </div>
+    <div className='fadein resume-inner-div'>
+      <div onClick={() => props.closeResume()} className='delete'>
+        <XIcon width={35} />
+      </div>
+      <div>
+        <Resume />
+        <span className='resume-close-link'>close</span>
       </div>
     </div>
   );
