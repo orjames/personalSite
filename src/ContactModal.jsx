59fd7aff09eff8087b7ec5a772f2314791a8a50a
orjames <orjames@berkeley.edu>
2019-04-23 11:15:25 -0700

    netlify contact form

--- src/ContactModal.jsx
+++ src/ContactModal.jsx
@@ -1,43 +1,128 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './CloseButton.css';
 
-const ContactModal = (props) => {
-  return (
-    <div className='form-div'>
-      <div className='modal-title-div'>contact</div>
-      <form name='contact' netlify>
-        <div className='input-div'>
-          <input
-            type='text'
-            name='name'
-            className='input'
-            placeholder='name...'
-          />
-        </div>
-        <div className='input-div'>
-          <input
-            type='email'
-            name='email'
-            className='input'
-            placeholder='email...'
-          />
-        </div>
-        <div className='input-div'>
-          <textarea
-            type='text'
-            name='message'
-            className='input'
-            placeholder='message...'
-          />
-        </div>
-        <div className='input-div'>
-          <button type='submit' className='btn btn-1 btn-1e'>
-            Send
-          </button>
-        </div>
-      </form>
-    </div>
-  );
-};
+class ContactModal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { name: '', email: '', message: '' };
+  }
+
+  handleSubmit = (e) => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...this.state }),
+    })
+      .then(() => alert('Success!'))
+      .catch((error) => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    return (
+      <div className='form-div'>
+        <div className='modal-title-div'>contact</div>
+        <form name='contact' data-netlify='true'>
+          <input type='hidden' name='form-name' value='contact' />
+          <div className='input-div'>
+            {/* <input
+              type='text'
+              name='name'
+              className='input'
+              placeholder='name...'
+            /> */}
+            <input
+              type='text'
+              name='name'
+              className='input'
+              placeholder='name...'
+              value={name}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className='input-div'>
+            {/* <input
+              type='email'
+              name='email'
+              className='input'
+              placeholder='email...'
+            /> */}
+            <input
+              type='email'
+              name='email'
+              className='input'
+              placeholder='email...'
+              value={email}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className='input-div'>
+            {/* <textarea
+              type='text'
+              name='message'
+              className='input'
+              placeholder='message...'
+            /> */}
+            <textarea
+              name='message'
+              className='input'
+              placeholder='message...'
+              value={message}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className='input-div'>
+            <button type='submit' className='btn btn-1 btn-1e' />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default ContactModal;
+
+// const encode = (data) => {
+//   return Object.keys(data)
+//       .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+//       .join("&");
+// }
+
+// class ContactForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { name: "", email: "", message: "" };
+//   }
+
+//   /* Here’s the juicy bit for posting the form submission */
+
+//   render() {
+//     const { name, email, message } = this.state;
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <p>
+//           <label>
+//             Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+//           </label>
+//         </p>
+//         <p>
+//           <label>
+//             Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+//           </label>
+//         </p>
+//         <p>
+//           <label>
+//             Message: <textarea name="message" value={message} onChange={this.handleChange} />
+//           </label>
+//         </p>
+//         <p>
+//           <button type="submit">Send</button>
+//         </p>
+//       </form>
+//     );
+//   }
+// }
